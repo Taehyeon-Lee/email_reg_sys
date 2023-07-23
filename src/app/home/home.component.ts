50393744bfadf684c1f9af6f1fa872e3b8704ce2
Taehyeon-Lee <82371827+Taehyeon-Lee@users.noreply.github.com>
2023-07-23 06:29:36 -0400

    adding missing files

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+/**
+ * This file is the component for the home page. It is body content of the webpage home page.
+ */
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
